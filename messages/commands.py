20908f10a76a9ae4e0a5ee2b94d5b842a2b47fc5
Sandesh Shahapur <sandeshshahapur@gmail.com>
2024-03-02 15:36:51 +0530

    fixed out of bounds error again, for sure this time

--- messages/commands.py
+++ messages/commands.py
@@ -1,6 +1,6 @@
 async def process_command(message, prefix):
     text = message.content[len(prefix):].split()
-    command = text[0]
+    command = text[0] if len(text) > 0 else ""
     optionals = text[1:] if len(text) > 1 else []
     print(command)
 
