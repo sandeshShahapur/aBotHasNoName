502db76bcaf27429a5169a15c5da2f83bf09c72b
Sandesh Shahapur <sandeshshahapur@gmail.com>
2024-03-08 19:34:52 +0530

    all setups moved to setup_hook() and db pool closing moved to close()

--- aBotHasNoName.py
+++ aBotHasNoName.py
@@ -6,63 +6,69 @@ from discord.ext import commands
 import os
 import json
 from dotenv import load_dotenv
-import logging
+import logging, logging.handlers
 from typing import List, Optional
 
 import asyncpg
 
+load_dotenv()
 class aBotHasNoName(commands.Bot):
     def __init__(
         self,
         *args,
         initial_extensions: List[str],
-        db_pool: asyncpg.Pool,
         **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self.db_pool = db_pool
         self.initial_extensions = initial_extensions
 
+
     async def setup_hook(self) -> None:
+        logger = logging.getLogger('discord')
+        logger.setLevel(logging.INFO)
+
+        handler = logging.handlers.RotatingFileHandler(
+            filename='discord.log',
+            encoding='utf-8',
+            maxBytes=32 * 1024 * 1024,  # 32 MiB
+            backupCount=5,  # Rotate through 5 files
+        )
+        dt_fmt = '%Y-%m-%d %H:%M:%S'
+        formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        
+        db_pool = await asyncpg.create_pool(dsn=os.getenv('DATABASE_URL'), min_size=16, max_size=32)
+        self.db_pool = db_pool
+
         for extension in self.initial_extensions:
             await self.load_extension(extension)
 
-        
+    async def on_ready(self) -> None:
+        print(f'{self.user} has connected to Discord!')
 
-async def main():
+    async def close(self) -> None:
+        await super().close()
+        await self.db_pool.close()
+
+        
+def main() -> None:
     with open('config.json', 'r') as f:
         config = json.load(f)
         default_prefix = config['default_prefix']
-    
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.INFO)
 
-    handler = logging.handlers.RotatingFileHandler(
-        filename='discord.log',
-        encoding='utf-8',
-        maxBytes=32 * 1024 * 1024,  # 32 MiB
-        backupCount=5,  # Rotate through 5 files
-    )
-    dt_fmt = '%Y-%m-%d %H:%M:%S'
-    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    intents = discord.Intents.all() #intent basically allows a bot to subscribe to specific buckets of events
+    initial_extensions = []
+    description = '''A bot that has no name'''
 
-    async with asyncpg.create_pool(dsn=os.getenv('DATABASE_URL'), min_size=16, max_size=32) as pool:
-        intents = discord.Intents.all() #intent basically allows a bot to subscribe to specific buckets of events
-        initial_extensions = []
-        description = '''A bot that has no name'''
-
-        print('the default prefix is:', default_prefix) #debugging
-        async with aBotHasNoName(
-            intents=intents,
-            description=description,
-            command_prefix=default_prefix,
-            initial_extensions=initial_extensions,
-            db_pool=pool
-        ) as bot: #our bot, the connection to discord (Client is a class, while client is an instance of that class)
-            load_dotenv()
-            await bot.start(os.getenv('TOKEN')) #run bot
+    bot = aBotHasNoName(
+        intents=intents,
+        description=description,
+        command_prefix=default_prefix,
+        initial_extensions=initial_extensions
+    ) 
+    bot.run(os.getenv('TOKEN')) #run bot
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
