0eef046ae83da4382f74a0385c922cf64de4f7f3
Sandesh Shahapur <sandeshshahapur@gmail.com>
2024-03-08 14:55:41 +0530

    rebased the code for bot configs, proper initialisation of database and to enable usage of cogs

--- aBotHasNoName.py
+++ aBotHasNoName.py
@@ -1,34 +1,68 @@
 # @client.event - to register an event
 # async await - https://www.reddit.com/r/learnprogramming/comments/tya94m/comment/i3qy6ig/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+import asyncio
 import discord
 from discord.ext import commands
 import os
 import json
 from dotenv import load_dotenv
 import logging
+from typing import List, Optional
 
+import asyncpg
 
-with open("config.json", "r") as json_file:
-    config = json.load(json_file)
+class aBotHasNoName(commands.Bot):
+    def __init__(
+        self,
+        *args,
+        initial_extensions: List[str],
+        db_pool: asyncpg.Pool,
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.db_pool = db_pool
+        self.initial_extensions = initial_extensions
 
-description = '''A bot that has no name'''
-intents = discord.Intents().all() #intent basically allows a bot to subscribe to specific buckets of events
-bot = commands.Bot(prefix='.', description=description, intents=intents) #our bot, the connection to discord (Client is a class, while client is an instance of that class)
+    async def setup_hook(self) -> None:
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
 
-# event that is run when our bot is online and ready to start being used
-@bot.event
-async def on_ready(): 
-    print('We have logged in as {0.user}'.format(bot))
+        
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user or message.author.bot:
-        return
+async def main():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+        default_prefix = config['default_prefix']
+    
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
 
-load_dotenv()
-token = os.getenv('TOKEN')
-if token is None:
-    print("TOKEN environment variable not set. Exiting...")
-else:
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    bot.run(token, log_handler=handler, log_level=logging.DEBUG) #running the bot by supplying our token which is like the password for our bot
+    handler = logging.handlers.RotatingFileHandler(
+        filename='discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
+    )
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    async with asyncpg.create_pool(dsn=os.getenv('DATABASE_URL'), min_size=16, max_size=32) as pool:
+        intents = discord.Intents.all() #intent basically allows a bot to subscribe to specific buckets of events
+        initial_extensions = []
+        description = '''A bot that has no name'''
+
+        print('the default prefix is:', default_prefix) #debugging
+        async with aBotHasNoName(
+            intents=intents,
+            description=description,
+            command_prefix=default_prefix,
+            initial_extensions=initial_extensions,
+            db_pool=pool
+        ) as bot: #our bot, the connection to discord (Client is a class, while client is an instance of that class)
+            load_dotenv()
+            await bot.start(os.getenv('TOKEN')) #run bot
+
+if __name__ == '__main__':
+    asyncio.run(main())
