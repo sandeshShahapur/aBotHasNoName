722c3cc6033479a894bdc526ce021338a5051433
Sandesh Shahapur <sandeshshahapur@gmail.com>
2024-03-14 09:29:56 +0530

    edits to allow forabstraction of channels, permissions, and roles

--- Cogs/Admin.py
+++ Cogs/Admin.py
@@ -3,10 +3,13 @@ import asyncio
 from discord.ext import commands
 from data.databases.events import get_default_role
 import json
+import asyncpg
+
 
 class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.ld_permissions = ["send_messages", "add_reactions", "create_public_threads", "create_private_threads", "send_messages_in_threads", "use_application_commands", "connect"] #TODO make this abstract
 
     #TODO permission checks
     @commands.is_owner()
@@ -23,54 +26,101 @@ class Admin(commands.Cog):
     
         #TODO make this flexible
         await ctx.send('Initiating lockdown sequence...')
+        targets = await self.get_targets(ctx, *args)
+        if not targets:
+            return
+
+        # *locking channels and setting up json data to remember which channel perms to unlock    
+        await ctx.send('Locking down channels...')
+        json_data = {}
+        json_data["targets"] = [target.id for target in targets]
         # *these are the permissions that will be removed from the id's
-        ld_permissions = ["send_messages", "add_reactions", "create_public_threads", "create_private_threads", "send_messages_in_threads", "use_application_commands", "connect"]
-        #if not users and not roles:
+        json_data["ld_permissions"] = self.ld_permissions
+        json_data["channels"] = []
+        
+        for channel in ctx.guild.text_channels + ctx.guild.voice_channels: #TODO make this abstract
+            json_channel = {"channel_id": channel.id, "targets": []}
+
+            for target in targets:
+                json_target = {"target_id": target.id, "permissions": []}
+                cur_perms = channel.overwrites_for(target)
+
+                for ld_permission in self.ld_permissions:
+                    if getattr(cur_perms, ld_permission) == False:
+                        # *channel is already locked, add to json
+                        json_target["permissions"].append(ld_permission)
+                        continue
+
+                    # *lock the channel permission
+                    setattr(cur_perms, ld_permission, False)
+                    await channel.set_permissions(target, overwrite=cur_perms)
+
+                if json_target["permissions"]:
+                    json_channel["targets"].append(json_target)
+
+            if json_channel["targets"]:
+                json_data["channels"].append(json_channel)
+
+        with open(f"data/json/lockdowns/{ctx.guild.name}_lockdown.json", "w") as f:
+            json.dump(json_data, f)
+        await ctx.send('Lockdown sequence complete...\n\n Good luck when unlocking...')
+
+
+    @commands.is_owner()
+    @commands.group()
+    async def unlock(self, ctx: commands.Context):
+        if ctx.invoked_subcommand is None:
+            #TODO implement default lockdown of context's channel
+            await ctx.send('Invalid unlock command passed...')
+
+        
+    '''@unlock.command()
+    async def maintainance(self, ctx: commands.Context, *args: str):
         if not args:
             default_role = await get_default_role(self.bot.db_pool, ctx.guild.id)
             targets = ctx.guild.get_role(default_role)
             if not targets:
                 await ctx.send('Aborting, no default role set for this server...')
                 return
-        else:
-            #users, roles = users.split(','), roles.split(',')
-            #targets = [ctx.guild.get_member(int(user)) for user in users] + [ctx.guild.get_role(int(role)) for role in roles]
-            targets = [ctx.guild.get_member(int(member)) for member in args]
 
-        await ctx.send('Locking down channels...')
-        # *locking channels and setting up json data to remember which channel perms to unlock    
-        json_data = {}
-        json_data["channels"] = []
-        for channel in ctx.guild.text_channels + ctx.guild.voice_channels: #TODO make this flexible
-            json_channel = {
-                            "channel_id": channel.id,
-                            "targets": []
-                            }
-            for target in targets:
-                json_target = {
-                                "target_id": target.id,
-                                "permissions": []
-                                }
-                cur_perms = channel.overwrites_for(target)
-                for ld_permission in ld_permissions:
-                    if getattr(cur_perms, ld_permission) == False:
-                        # *channel is already locked, add to json
-                        json_target["permissions"].append(ld_permission)
-                        continue
-                    # *lock the channel permission
-                    setattr(cur_perms, ld_permission, False)
-                    await channel.set_permissions(target, overwrite=cur_perms)
-                if json_target["permissions"]:
-                    json_channel["targets"].append(json_target)
-            if json_channel["targets"]:
-                json_data["channels"].append(json_channel)
-        # *save json data if any channels were already locked
-        if json_data["channels"]:
-            with open(f"data/json/lockdowns/{ctx.guild.name}_lockdown.json", "w") as f:
-                json.dump(json_data, f)
-        await ctx.send('Lockdown sequence complete...\n\n Good luck when unlocking...')
-           
+            await ctx.send('Lockdown to terminate unspecified...')
+        
+        if args[0] == "maintainance":
+            # *unlocking channels and setting up json data to remember which channel perms to lock
+            await ctx.send('Terminating maintainance lockdown...')
+            await ctx.send('Unlocking channels...')
+            with open(f"data/json/lockdowns/{ctx.guild.name}_lockdown.json", "r") as f:
+                json_data = json.load(f)
+
+            for channel in ctx.guild.text_channels + ctx.guild.voice_channels:
+                for 
+
+            await ctx.send('Unlock sequence complete...')
+            await ctx.send('Maintainance lockdown terminated...')
+            await self.bot.change_presence(status=discord.Status.online, activity=discord.Game('.help and your mom'))
+            await ctx.send("Hoping perms aren't fu*ked.")'''
                 
+
+    async def get_targets(self, ctx: commands.Context, *args):
+        if not args:
+            default_role = await get_default_role(self.bot.db_pool, ctx.guild.id)
+            targets = ctx.guild.get_role(default_role)
+            if not targets:
+                await ctx.send('Aborting, no default role set for this server...')
+                return None
+        else:
+            targets = []
+            for target in args:
+                if target.isdigit():
+                    target = ctx.guild.get_role(int(target)) or ctx.guild.get_member(int(target))
+                    if target:
+                        targets.append(target)
+
+            if not targets:
+                await ctx.send('Aborting, no valid targets found...')
+                return None
+            else:
+                return targets
 
 
 async def setup(bot: commands.Bot):
